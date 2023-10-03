c2baae4753492215b3c553740fa228751a87732a
Jill Allan <54241079+jillallan@users.noreply.github.com>
2023-10-03 17:06:26 +0100

    Initial Commit

---
+++ Old/OldApp.swift
@@ -0,0 +1,17 @@
+//
+//  OldApp.swift
+//  Old
+//
+//  Created by Jill Allan on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct OldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
